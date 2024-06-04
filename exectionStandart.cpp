435e546c92d2fc6cbd49119a157ba01f810a6d0e
dzakyputra45 <dzakypratama45@gmail.com>
2024-06-04 15:10:32 +0700

    Membuat exception handling try untuk membuat program

---
+++ exectionStandart.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <exception>
+//untuk objek exeception yang akan digunakan
+#include <array>
+//untuk objek array yang akan kita gunakan
+using namespace std;
+
+int main()
+{
+    cout << "Awal Program" << endl; //penanda 1:...
+    try {
+        array<int, 3> data = { 3, 5, 7 };
+        //pesan array integer 3 elemen
+        cout << data.at(5)<< endl;
+        //memanggil array elemen ke 5
+    }
+    
+}
